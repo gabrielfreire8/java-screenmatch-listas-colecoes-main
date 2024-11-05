ce9a14966e6017dac3794a5d2ac740cd19d610d1
Gabriel Freire <gabrielmfreire2506@gmail.com>
2024-11-05 17:25:35 -0300

    .

---
+++ src/br/com/principal/PrincipalComListas.java
@@ -0,0 +1,52 @@
+package br.com.principal;
+
+import br.com.screenmatch.modelos.Filme;
+import br.com.screenmatch.modelos.Serie;
+import br.com.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class PrincipalComListas {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        meuFilme.avalia(9);
+        Serie lost = new Serie("Lost", 2000);
+        var filmeDoPaulo = new Filme("Dogville", 2003 );
+        meuFilme.avalia(7);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        meuFilme.avalia(8);
+
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(filmeDoPaulo);
+        lista.add(meuFilme);
+        lista.add(outroFilme);
+        lista.add(lost);
+        for (Titulo item: lista){
+            System.out.println(item.getNome());
+            if (item instanceof Filme filme){
+                System.out.println("Classificação" + filme.getClassificacao());
+
+            }
+
+            ArrayList<String> buscaPorArtista = new ArrayList<>();
+            buscaPorArtista.add("Adam sandler");
+            buscaPorArtista.add("Jason Momoa");
+            buscaPorArtista.add("Johnny Deep");
+            System.out.println(buscaPorArtista);
+
+            Collections.sort(buscaPorArtista);
+            System.out.println("Depois da ordenação");
+            System.out.println(buscaPorArtista);
+        System.out.println("Lista de titulos ordenadas");
+            Collections.sort(lista);
+            System.out.println(lista);
+            lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+            System.out.println(lista);
+        }
+
+
+    }
+}
